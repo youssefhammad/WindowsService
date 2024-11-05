58313bb3f2063043912c5c08d0526abe53b2744e
Youssef Hamad <youssef@technomedic.com>
2024-11-05 11:39:12 +0200

    Add project files.

---
+++ DatabaseScriptService/DatabaseScriptService.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace DatabaseScriptService
+{
+    partial class DatabaseScriptService
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3600000;
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.Log = "Application";
+            this.eventLog1.Source = "DatabaseScriptService";
+            // 
+            // DatabaseScriptService
+            // 
+            this.ServiceName = "DatabaseScriptService";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer2;
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
---
+++ DatabaseScriptService/DatabaseScriptService.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.IO;
+using System.Timers;
+
+namespace DatabaseScriptService
+{
+    public partial class DatabaseScriptService : ServiceBase
+    {
+        private Timer timer1 = null;
+        public DatabaseScriptService()
+        {
+            InitializeComponent();
+
+            // Instantiate eventLog1 if not using Designer
+            eventLog1 = new EventLog();
+            if (!EventLog.SourceExists("DatabaseScriptService"))
+            {
+                EventLog.CreateEventSource("DatabaseScriptService", "Application");
+            }
+            eventLog1.Source = "DatabaseScriptService";
+            eventLog1.Log = "Application";
+
+            // Initialize the timer if not using Designer
+            timer1 = new Timer();
+            timer1.Interval = 3600000; // 1 hour
+            timer1.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer1.AutoReset = true;
+            timer1.Enabled = false; // We'll enable this in OnStart
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // Log the service start event
+            eventLog1.WriteEntry("DatabaseScriptService started.");
+
+            // Execute immediately when service starts
+            ExecuteDatabaseScript();
+
+            // Enable timer for subsequent executions
+            timer1.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+
+            // Log the service stop event
+            eventLog1.WriteEntry("DatabaseScriptService stopped.");
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            eventLog1.WriteEntry("OnElapsedTime event fired. Starting script execution.", EventLogEntryType.Information);
+
+            ExecuteDatabaseScript();
+        }
+
+        private void ExecuteDatabaseScript()
+        {
+            try
+            {
+                // Define your connection string
+                string connectionString = @"Server=DESKTOP-IGHTSU5\SQLEXPRESS;Database=test90;User Id=admin;Password=admin;TrustServerCertificate=True;multipleactiveresultsets=True";
+                // Read the SQL script from a file
+                string script = File.ReadAllText(@"F:\WindowsServicePOC.sql");
+
+                // Normalize "GO" statements
+                script = script.Replace("\r\n", "\n"); // Normalize line endings
+                var scriptLines = script.Split(new[] { '\n' }, StringSplitOptions.None);
+                StringBuilder sqlBuilder = new StringBuilder();
+                List<string> commandList = new List<string>();
+
+                foreach (var line in scriptLines)
+                {
+                    if (line.Trim().Equals("GO", StringComparison.OrdinalIgnoreCase))
+                    {
+                        // End of batch
+                        if (sqlBuilder.Length > 0)
+                        {
+                            commandList.Add(sqlBuilder.ToString());
+                            sqlBuilder.Clear();
+                        }
+                    }
+                    else
+                    {
+                        sqlBuilder.AppendLine(line);
+                    }
+                }
+
+                // Add any remaining SQL statements
+                if (sqlBuilder.Length > 0)
+                {
+                    commandList.Add(sqlBuilder.ToString());
+                }
+
+                // Execute each command
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    foreach (string commandText in commandList)
+                    {
+                        string trimmedCommand = commandText.Trim();
+                        if (trimmedCommand.Length > 0)
+                        {
+                            using (SqlCommand cmd = new SqlCommand(trimmedCommand, conn))
+                            {
+                                cmd.ExecuteNonQuery();
+                            }
+                        }
+                    }
+                }
+
+                // Log success
+                eventLog1.WriteEntry("Database script executed successfully.", EventLogEntryType.Information);
+            }
+            catch (Exception ex)
+            {
+                // Log any errors
+                string errorMsg = $"Error executing database script: {ex.Message}\n{ex.StackTrace}";
+                eventLog1.WriteEntry(errorMsg, EventLogEntryType.Error);
+            }
+        }
+    }
+}
